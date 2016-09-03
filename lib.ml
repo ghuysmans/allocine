05edeb5633a932f67d1b9c3071e03615a2d1c7aa
Guillaume Huysmans <ghuysmans99@gmail.com>
2016-09-03 19:22:42 +0200

    Implemented a file download function

--- lib.ml
+++ lib.ml
@@ -15,3 +15,19 @@ let string_of_int_o = function
 | None -> "<none>"
 
 exception HttpError of int
+
+let download url destination =
+    let open Lwt in
+    let open Cohttp in
+    let open Cohttp_lwt_unix in
+    let open Net_config in
+    let headers = Header.init_with "User-Agent" user_agent in
+    Client.call ~headers `GET (Uri.of_string url) >>= fun (resp, body) ->
+    let status = Response.status resp in
+    match status with
+    | `OK -> Cohttp_lwt_body.to_string body >>= fun s ->
+        let c = open_out destination in
+        output c s 0 (String.length s);
+        close_out c;
+        return ()
+    | _ -> raise (HttpError (Code.code_of_status status))
