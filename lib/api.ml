78e0d9edd6bf74e42084dc1f32220fa614a92c9e
Guillaume Huysmans <ghuysmans99@gmail.com>
2019-09-30 15:55:43 +0200

    Documented the Api module

--- lib/api.ml
+++ lib/api.ml
@@ -1,9 +1,5 @@
-let sed () =
-  let open Unix in
-  let t = localtime (time ()) in
-  Printf.sprintf "%d%02d%02d" (t.tm_year+1900) (t.tm_mon+1) t.tm_mday
-
-module type C = sig
+(** API configuration: keys, etc. *)
+module type CONFIG = sig
   val api_url : string
   val partner_key : string
   val private_key : string
@@ -12,31 +8,9 @@ end
 
 exception HttpError of int
 
-module Make (CONFIG : C) (CLIENT : Cohttp_lwt.S.Client) = struct
-  let call service_method parameters =
-    let open Lwt.Infix in
-    let signed =
-      Uri.encoded_of_query @@
-        ("sed", [sed ()]) ::
-        ("partner", [CONFIG.partner_key]) ::
-        List.map (fun (a,b) -> (a, [b])) parameters
-    in
-    let signature =
-      CONFIG.private_key ^ signed |>
-      Digestif.SHA1.digest_string |>
-      Digestif.SHA1.to_raw_string |>
-      B64.encode
-    in
-    let headers = Cohttp.Header.init_with "User-Agent" CONFIG.user_agent in
-    CONFIG.api_url ^ service_method ^ "?" ^ signed ^ "&sig=" ^ signature |>
-    Uri.of_string |>
-    CLIENT.get ~headers >>= fun (resp, body) ->
-    match Cohttp.Response.status resp with
-    | `OK ->
-      Cohttp_lwt.Body.to_string body
-    | status ->
-      Lwt.fail (HttpError (Cohttp.Code.code_of_status status))
-
+module Make (C : CONFIG) (H : Cohttp_lwt.S.Client) = struct
+  (** [call service params] calls a [service] with the given [params],
+      caching the answer. *)
   let call ?bypass service_method parameters =
     let lifetime = Maki.(Lifetime.KeepFor (Time.days 5)) in
     let args = Maki.(Arg.[
@@ -45,22 +19,52 @@ module Make (CONFIG : C) (CLIENT : Cohttp_lwt.S.Client) = struct
     ]) in
     let returning = Maki.Codec.string in
     Maki.call_pure ?bypass ~lifetime ~name:"allocine" ~args ~returning @@
-    fun () -> call service_method parameters
+    fun () ->
+      let open Lwt.Infix in
+      let signed =
+        Uri.encoded_of_query @@
+          ("sed", [
+            let open Unix in
+            let t = localtime (time ()) in
+            Printf.sprintf "%d%02d%02d"
+              (t.tm_year+1900) (t.tm_mon+1) t.tm_mday
+          ]) ::
+          ("partner", [C.partner_key]) ::
+          List.map (fun (a,b) -> (a, [b])) parameters
+      in
+      let signature =
+        C.private_key ^ signed |>
+        Digestif.SHA1.digest_string |>
+        Digestif.SHA1.to_raw_string |>
+        B64.encode
+      in
+      let headers = Cohttp.Header.init_with "User-Agent" C.user_agent in
+      C.api_url ^ service_method ^ "?" ^ signed ^ "&sig=" ^ signature |>
+      Uri.of_string |>
+      H.get ~headers >>= fun (resp, body) ->
+      match Cohttp.Response.status resp with
+      | `OK ->
+        Cohttp_lwt.Body.to_string body
+      | status ->
+        Lwt.fail (HttpError (Cohttp.Code.code_of_status status))
 
   open Maki.E.Infix
 
+  (** [search q max] looks for [q], returning at most [max] results. *)
   let search query max_results =
     let p = ["q", query; "count", string_of_int max_results] in
     call "search" (("format", "json") :: p) >|=
     Atdgen_runtime.Util.Json.from_string Types_j.read_search >|= fun s ->
     s.Types_t.feed
 
+  (** [movie id] returns the movie with the given [id]. *)
   let movie id =
     let p = [("code", string_of_int id); ("profile", "large")] in
     call "movie" (("format", "json") :: p) >|=
     Atdgen_runtime.Util.Json.from_string Types_j.read_get_movie >|= fun m ->
     m.Types_t.movie
 
+  (** [person id] returns the person with the given [id]. *)
   let person id =
     let p = [("code", string_of_int id); ("profile", "large")] in
     call "person" (("format", "json") :: p) >|=
@@ -68,12 +72,12 @@ module Make (CONFIG : C) (CLIENT : Cohttp_lwt.S.Client) = struct
     m.Types_t.person
 end
 
-(* FIXME? *)
-let clean_director s =
+(** [clean name] removes small Roman numbers at the end of [name] (FIXME?). *)
+let clean name =
   let re = Re.(compile (seq [
     group (rep any);
     blank; char '('; rep (alt [char 'V'; char 'I']); char ')'; eos
   ])) in
-  match Re.exec_opt re s with
-  | None -> s
+  match Re.exec_opt re name with
+  | None -> name
   | Some g -> Re.Group.get g 1
