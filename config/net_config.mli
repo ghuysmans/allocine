0fa387c6e6f1cc9e602dc5e726cf18cbfd20f2d4
Guillaume Huysmans <ghuysmans99@gmail.com>
2020-04-19 19:31:56 +0200

    Added a config skeleton for the demo

---
+++ config/net_config.mli
@@ -0,0 +1,1 @@
+include Allocine.Api.CONFIG
---
+++ config/net_config.sample.ml
@@ -0,0 +1,4 @@
+let api_url = "http://api.allocine.fr/rest/v3/"
+let user_agent = "user-agent"
+let partner_key = "partner key"
+let private_key = "private key"
